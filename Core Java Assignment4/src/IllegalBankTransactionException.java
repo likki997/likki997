6efc1eda9699ea9c0558a371c882071f55426226
likki997 <95916502+likki997@users.noreply.github.com>
2021-12-23 22:00:14 +0530

    Add files via upload

---
+++ Core Java Assignment4/src/IllegalBankTransactionException.java
@@ -0,0 +1,18 @@
+
+	
+	public class IllegalBankTransactionException extends Exception
+	{
+		public IllegalBankTransactionException()
+		{
+		super();
+		}
+		public IllegalBankTransactionException(String msg)
+		{
+		super(msg);
+		}
+
+
+
+		}
+
+
---
+++ Core Java Assignment4/src/InsufficientBalanceException.java
@@ -0,0 +1,16 @@
+
+public class InsufficientBalanceException  extends Exception {
+		public InsufficientBalanceException()
+		{
+		super();
+
+		}
+		public InsufficientBalanceException(String msg)
+		{
+		super(msg);
+		}
+
+
+
+		}
+
