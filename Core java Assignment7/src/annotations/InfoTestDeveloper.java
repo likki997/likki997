6ca8ad725d390f40b5f0904cba023393c7ba0d3f
likki997 <95916502+likki997@users.noreply.github.com>
2021-12-23 22:02:15 +0530

    Add files via upload

---
+++ Core java Assignment7/src/annotations/InfoTestDeveloper.java
@@ -0,0 +1,68 @@
+package annotations;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+
+
+
+
+import java.sql.Date;
+import java.sql.Time;
+
+
+
+@Retention(RUNTIME)
+@Target(METHOD)
+@interface info{
+
+}
+
+
+
+public class InfoTestDeveloper{
+@info
+public void authorid(int auth_id) {
+System.out.println("authorid"+auth_id);
+
+}
+@info
+public void author(String author) {
+System.out.println("author"+author);
+
+}
+@info
+public void date(java.util.Date date) {
+System.out.println("Date"+date);
+
+
+
+}
+@info
+public void time(Time time) {
+System.out.println("time"+time);
+}
+@info
+public void version(double d) {
+System.out.println("Version"+d);
+}
+public static void main(String[] args) {
+	InfoTestDeveloper info=new InfoTestDeveloper();
+info.authorid(1);
+info.author("balu");
+info.date(new java.util.Date());
+info.time(new java.sql.Time(0));
+info.version(1.8);
+
+
+
+
+}
+
+
+}
+
+
+
+
