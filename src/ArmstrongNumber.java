3cc4f2292625227d0f8abd254e6f8941b9a2ca42
likki997 <95916502+likki997@users.noreply.github.com>
2021-12-23 21:43:53 +0530

    Add files via upload

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,24 @@
+
+public class ArmstrongNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int number=153,number2,remainder,result=0;
+number2=number;
+while(number2!=0)
+{
+remainder=number2%10;
+result+=Math.pow(remainder, 3);
+number2 /=10;
+}
+if(result == number)
+System.out.println(number + "is an Armstrong number");
+else
+System.out.println(number + "is not an Armstrong number");
+}
+
+
+
+	}
+
+
---
+++ src/CuiLoginPage.java
@@ -0,0 +1,27 @@
+
+	 import java.util.Scanner;
+	public class CuiLoginPage{
+		
+	
+	
+		public static void main(String[] args) {
+	try (Scanner input = new Scanner(System.in)) {
+		String user,pass;
+		System.out.println("enter your username: ");
+		user=input.nextLine();
+		System.out.println("enter your password");
+		pass=input.nextLine();
+		if(user.equals("abc") && (pass.equals("1234")))
+		{
+		System.out.println("welcome abc");
+		}
+		else
+		{
+		System.out.println("contact admin");
+		}
+	}
+	}}
+
+
+
+
