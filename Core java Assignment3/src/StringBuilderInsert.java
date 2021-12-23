c43d280ebe3607f931b0d492bd59f1eb8a515ef7
likki997 <95916502+likki997@users.noreply.github.com>
2021-12-23 21:59:33 +0530

    Add files via upload

---
+++ Core java Assignment3/src/StringBuilderInsert.java
@@ -0,0 +1,9 @@
+
+public class StringBuilderInsert {
+	public static void main(String[] args) {
+		StringBuilder sb= new StringBuilder("It is used to at the specified index position");
+		sb.insert(14, "insert text ");
+		System.out.println(sb); 
+
+}
+}
---
+++ Core java Assignment3/src/Stringinsert.java
@@ -0,0 +1,9 @@
+
+public class Stringinsert {
+	public static void main(String[] args) {
+		StringBuffer sb= new StringBuffer("It is used to at the specified index position");
+		sb.insert(14, "insert text ");
+		System.out.println(sb); 
+
+}
+}
