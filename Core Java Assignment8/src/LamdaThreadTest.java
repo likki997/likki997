86d267aeed32f5ace7cd48ec137e53dd1cc559be
likki997 <95916502+likki997@users.noreply.github.com>
2021-12-24 13:11:46 +0530

    Add files via upload

---
+++ Core Java Assignment8/src/LamdaThreadTest.java
@@ -0,0 +1,28 @@
+
+public class LamdaThreadTest {
+	 public static void main(String[] args) {
+		// Child thread
+		new Thread(() -> { // Lambda Expression
+		for(int i=1; i <= 5; i++) {
+		System.out.println("Child Thread: "+ i);
+		try {
+		Thread.sleep(500);
+		} catch(Exception e) {
+		e.printStackTrace();
+		}
+		}
+		}).start();
+		// Main Thread
+		for(int j=1; j < 5; j++) {
+		System.out.println("Main Thread: "+ j);
+		try {
+		Thread.sleep(500);
+		} catch(Exception e) {
+		e.printStackTrace();
+		}
+		}
+		}
+		}
+
+
+
